e9ca4f2fdf8c00c60aa644c13ed4c1c092e8ec58
Nermeen <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2019-07-21 16:52:01 +0400

    initFrameWork

---
+++ NWAuthVIP/NWAuthVIP/NWAuthVIP.h
@@ -0,0 +1,19 @@
+//
+//  NWAuthVIP.h
+//  NWAuthVIP
+//
+//  Created by iOS on 7/21/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NWAuthVIP.
+FOUNDATION_EXPORT double NWAuthVIPVersionNumber;
+
+//! Project version string for NWAuthVIP.
+FOUNDATION_EXPORT const unsigned char NWAuthVIPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NWAuthVIP/PublicHeader.h>
+
+
---
+++ NWAuthVIP/NWAuthVIP/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  NWAuthVIP
+//
+//  Created by iOS on 7/21/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    public static func doSome() -> String
+    {
+        return "Nermeen"
+    }
+    
+}
