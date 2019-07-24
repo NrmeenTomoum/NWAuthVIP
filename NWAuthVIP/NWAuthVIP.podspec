9a942a3330b8416e6bddcc6c283e2d961136f6d4
Nermeen <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2019-07-24 14:28:06 +0400

    c1

---
+++ NWAuthVIP/NWAuthVIP.podspec
@@ -0,0 +1,27 @@
+Pod::Spec.new do |spec|
+
+  spec.name         = "NWAuthVIP"
+  spec.version      = "1.0.0"
+  spec.summary      = "NWAuthVIP"
+ spec.swift_version = '5.0'
+
+  spec.description  = "NWAuthVIP is NEtWork Layer and Authentication and VIP Architecture"
+
+  spec.homepage     = "https://github.com/NrmeenTomoum/NWAuthVIP"
+
+   spec.license = { :type => 'Copyright', :text => <<-LICENSE
+                   Copyright 2018
+                   Permission is granted to...
+                  LICENSE
+                }
+
+  spec.author             = { "Nermeen" => "nrmeen.tomoum@alumni.fci-cu.edu.eg" }
+
+    spec.platform     = :ios, "12.2"
+
+
+  spec.source       = { :git => "https://github.com/NrmeenTomoum/NWAuthVIP.git", :tag => "1.0.0" }
+
+  spec.source_files  = "NWAuthVIP/**/*.{h,m,swift}"
+
+end
--- NWAuthVIP/NWAuthVIPTests/NWAuthVIPTests.swift
+++ NWAuthVIP/NWAuthVIPTests/NWAuthVIPTests.swift
@@ -2,7 +2,7 @@
 //  NWAuthVIPTests.swift
 //  NWAuthVIPTests
 //
-//  Created by iOS on 7/24/19.
+//  Created by iOS on 7/23/19.
 //  Copyright © 2019 iOS. All rights reserved.
 //
 
@@ -10,27 +10,25 @@ import XCTest
 @testable import NWAuthVIP
 
 class NWAuthVIPTests: XCTestCase {
-    
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
